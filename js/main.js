a4675a45097b4d2a460dcdb803b339e06184215a
Mustafo-programmer <am01102005mo@gmail.com>
2022-09-21 14:06:47 +0300

    first proect javascript

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var Name = prompt("ismingiz😊:   ");
+console.log(Name);
+var surname = prompt("familiyangiz😂:   ");
+console.log(surname);
+var age = prompt("yoshingiz😁:   ");
+console.log(age);
+var both_years = prompt("Tug'ulgan yilingiz🎂:   ");
+console.log(both_years);
+var number = prompt("telefon☎:  ");
+console.log(number);
+var hobby = prompt("Qiziqishingiz👌:   ");
+console.log(hobby);
+var car = prompt("Mashinangiz🚗:  ");
+console.log(car);
+var adres = prompt("Manzilingiz💒: ");
+console.log(adres);
+var  work = prompt("Ishingiz🤔:");
+console.log(work);
+var money = prompt("Oyligingiz💵:");
+console.log(money);
+var orzu = prompt("Kelajakka orzuyingiz🤔:");
+console.log(orzu);
+var bolalar_soni = prompt("Bolalaringiz soni🙅‍♀️🙅‍♂️:");
+console.log(bolalar_soni);
+
+var hammasi = [
+    "Ismingiz😊:  " + " " + Name + "\n"+
+    "Familiyangiz😂:  " + " " + surname + "\n"+
+    "Yoshingiz😁:  " + " " + age + "\n"+
+    "Tug'ulgan yilingiz🎂:  " + " " + both_years + "\n"+
+    "Telefon raqamingiz☎:  " + " " + number + "\n"+
+    "Qiziqishingiz👌:  " + " " + hobby + "\n"+
+    "Mashingiz💒:  " + " " + car + "\n" +
+    "Manzilingiz🚗:  " + " " + adres + "\n" +
+    "Ishingiz🤔:  " + " " + work + "\n" +
+    "Oyligingiz💵:  " + " " + money + "\n" +
+    "Kelajakka ozuyingiz🤔:  " + " " + orzu + "\n" +
+    "Bolalaringiz soni🙅‍♀️🙅‍♂️:  " + " " + bolalar_soni + "\n" 
+]
+alert(hammasi)
+
+
+
+
+
+
+
+
+// var text = "hello world";
+
+// console.log(text)
